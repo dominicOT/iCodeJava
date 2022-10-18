9dd1b83eb55382532d6907742e5bd986ddb89549
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-10-18 17:57:29 +0100

    Add files via upload

---
+++ nwArt.java
@@ -0,0 +1,18 @@
+public class nwArt {
+    public static void main (String args[]) {
+
+
+System.out.println("                   /@@@@@@@@@@@@@@@/                     ");
+System.out.println("            .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               ");
+System.out.println("        #@@@@@@@@@@@@             @@@@@@@@@@@@#          ");
+System.out.println("     @@@@@@   (@@@@        @@@@@    @@@@(   @@@@@@       ");
+System.out.println("   @@@@@      @@@           @@@@@@    @@@      @@@@@     ");
+System.out.println(" @@@@(       @@@@   @@    @@@@@@@@@   @@@@       (@@@@   ");
+System.out.println("  @@@@@      &@@@   /@@@@@@@@@@@@@(   @@@@      @@@@@    ");
+System.out.println("    @@@@@&    @@@@    @@@@@@@@@@@    @@@@    @@@@@@      ");             
+System.out.println("       @@@@@@/ @@@@@     ,@@@,     @@@@@ *@@@@@@         ");
+System.out.println("          #@@@@@@@@@@@@         @@@@@@@@@@@@%            ");
+System.out.println("                *@@@@@@@@@@@@@@@@@@@@@@@*                ");
+        
+    }
+}
