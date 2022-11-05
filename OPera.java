7d040208278b2897db4de364dc3f6b8a01da0fb8
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2022-11-05 13:12:39 +0100

    Add files via upload

---
+++ OPera.java
@@ -0,0 +1,16 @@
+public class OPera { //filename must be the same with the class name
+    public static void main (String args[]) {
+
+        int var1 = 600;
+        int var2 = 500;
+
+        int sum = var1 + var2;
+        System.out.println(sum); //we shouldn't use wrong statements
+
+        // int sum = var1 - var2;
+        // int sum = var1 * var2;
+        // int sum = var1 / var2;
+        // int sum = var1 % var2;
+        
+    }
+} //make sure to close the blocks
