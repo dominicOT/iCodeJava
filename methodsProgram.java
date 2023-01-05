83b922fe63d00ed1c154439dc713e1c37e578241
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-01-05 18:03:38 +0100

    Add files via upload

---
+++ methodsProgram.java
@@ -0,0 +1,29 @@
+public class methodsProgram {
+
+static void javameth {
+int test = 16;
+int exam = 70;
+int sum = test + exam;
+System.out.prinltn("Your final for Javascore is: " +sum);
+}
+
+static void webmeth {
+int test = 14;
+int exam = 69;
+int sum = test + exam;
+System.out.prinltn("Your final score for Web Programming is: " +sum);
+}
+
+static void pymeth {
+int test = 20;
+int exam = 72;
+int sum = test + exam;
+System.out.prinltn("Your final score for Python is: " +sum);
+}
+
+public static void main (String args[]) {
+javameth();
+webmeth();
+pymeth();
+}
+}
