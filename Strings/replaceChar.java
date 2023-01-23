f7d8c3895258cb20eef74b2d1879cef74562f8fc
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-01-23 12:47:16 +0100

    Create replaceChar.java

---
+++ Strings/replaceChar.java
@@ -0,0 +1,9 @@
+class replaceChar {
+    public static void main(String[] args) {
+        String name_str = "dominic_OT";
+        String new_str = name_str.replace("_", "");
+        
+        System.out.println("Initial String : " + name_str);
+        System.out.println("Modified String : " + new_str);
+    }
+}
