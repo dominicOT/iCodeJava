63bc48a1ded22b3cdb8b287e36b2e46837b926b1
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-20 01:54:01 +0100

    for and while loop

---
+++ loops/forWhile.java
@@ -0,0 +1,20 @@
+public class forWhile {
+    public static void main(String[] args) {
+    //for loop
+        System.out.println("Using for loop:");
+      
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Iteration \n" + i);
+        }
+        
+    //while loop
+        System.out.println("Using while loop:");
+      
+        int j = 1;
+      
+        while (j <= 15) {
+            System.out.println("Iteration " + j);
+            j++;
+        }
+    }
+}
